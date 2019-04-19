0b60904b10c5fdb225aa9b911bb151bb87abf435
waldekglaz <waldekglaz@gmail.com>
2019-04-19 20:47:40 +0100

    clock

---
+++ script/script.js
@@ -0,0 +1,20 @@
+// Set a deadline date
+
+var deadline = '31 Dec 2019';
+
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  console.log(getTimeRemaining(deadline).minutes + ' ' + getTimeRemaining(deadline).seconds);
