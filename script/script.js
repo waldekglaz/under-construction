911c94678b605950297dcc32d4f5cad28d2474ab
waldekglaz <waldekglaz@gmail.com>
2019-04-20 19:07:49 +0100

    js clock
    Clockdown works but needs attention

--- script/script.js
+++ script/script.js
@@ -1,20 +1,31 @@
-// Set a deadline date
 
-var deadline = '31 Dec 2019';
 
-function getTimeRemaining(endtime){
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor( (t/1000) % 60 );
-    var minutes = Math.floor( (t/1000/60) % 60 );
-    var hours = Math.floor( (t/(1000*60*60)) % 24 );
-    var days = Math.floor( t/(1000*60*60*24) );
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
+function countdown(){
+  var now = new Date();
+  var eventDate = new Date(2019, 5, 12 );
+ 
+  var currentTime = now.getTime();
+  var eventTime = eventDate.getTime();
+  
+  var remTime = eventTime - currentTime;
+  
+  var s = Math.floor((remTime / 1000) % 60);
+  var m = Math.floor((remTime/1000/60)%60);
+  var h = Math.floor(remTime/(1000*60*60)% 24);
+  var d = Math.floor(remTime/(1000*60*60*24));
 
-  console.log(getTimeRemaining(deadline).minutes + ' ' + getTimeRemaining(deadline).seconds);
+  h = (h < 10) ? '0' + h : h;
+  m = (m < 10) ? '0' + m : m;
+  s = (s < 10) ? '0' + s : s;
+
+  document.getElementById('days').textContent = d;
+  document.getElementById('days').innerText = d;
+
+  document.getElementById('hours').textContent = h;
+  document.getElementById('minutes').textContent = m;
+  document.getElementById('seconds').textContent = s;
+
+  setTimeout(countdown, 1000);
+}
+
+countdown();
